4622272c9d9838dcbac38df2a783347cbbc35397
Somnath Pandey <44345876+cssp007@users.noreply.github.com>
2020-04-18 05:45:35 +0530

    changed

--- git.groovy
+++ git.groovy
@@ -1,7 +1,7 @@
 def getCodeFromGithub() {
     def github_URL = "https://github.com/cssp007/forjenkins.git"
-        if ('${github_URL}' == "https://github.com/cssp007/forjenkins.git") {
-                 git '${github_URL}'
+        if ("${github_URL}" == "https://github.com/cssp007/forjenkins.git") {
+                 git "${github_URL}"
         }
         else {
                  echo "Github URL is NOT match"
